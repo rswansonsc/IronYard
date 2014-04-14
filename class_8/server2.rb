c2d7b40124ac5eb6fed2db79f4cb5ffa9116218e
Roger Swanson <rswansonsc@gmail.com>
2014-04-13 23:57:57 -0400

    week 2 udpates

--- class_8/server2.rb
+++ class_8/server2.rb
@@ -34,4 +34,19 @@ end
 get '/about_us' do
 
 	erb :about_us, layout: :index
+
+	@happy_client = []
+10.times do
+	@happy_client.push(
+{person_name:             Faker::Name.name,
+person_email:            Faker::Internet.email,
+person_company:          Faker::Company.name,
+person_company_slogan:   Faker::Company.bs,
+person_phone:            Faker::PhoneNumber.phone_number,
+person_address:          Faker::Address.street_address,
+person_city:             Faker::Address.city,
+person_state:            Faker::Address.state,
+person_zip:              Faker::Address.zip_code})
+end
+
 end
