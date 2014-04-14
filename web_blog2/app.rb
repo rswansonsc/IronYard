d7a7b1d8e27eeebf41e660937a3679b5508e083c
Roger Swanson <rswansonsc@gmail.com>
2014-04-14 18:16:24 -0400

    updates from Week3 Day1

--- web_blog2/app.rb
+++ web_blog2/app.rb
@@ -1,22 +1,38 @@
 require 'sinatra'
-require 'faker'
+require 'data_mapper'
+require 'shotgun'
+
+DataMapper.setup(
+  :default,
+  'mysql://root@localhost/blog'  
+)
+# If you had a password:
+# database://username:password@location/database_name
+
+# Model
+class Blog
+  include DataMapper::Resource
+
+  # MySQL automatically gives EVERY object an ID
+  # Migration: Whenever you change a field inside of a database
+  # property: this is a keyword given to us from our DataMapper gem
+  property :id, Serial
+  property :blog_date, String
+  property :blog_title, String
+  property :blog_content, String
+  # property :blog_picture, String
+end
+
+DataMapper.finalize.auto_upgrade!
 
 
 get '/' do
-	erb :index, layout: :index
-end
-get '/about' do
-	erb :about, layout: :about
-end
-get '/blog' do
+  @blog = Blog.all
 	erb :blog, layout: :blog_home_1
 end
+get '/blog' do
+	erb :blog, layout: :blog
+end
 get '/index' do
-  erb :index, layout: :index
-end
-get '/other' do
-	erb :other, layout: :full_width
-end
-get '/services' do
-  erb :services, layout: :services
+  erb :blog_post, layout: :blog_post
 end
