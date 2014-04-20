92e0e1aab07c65b8415ff5cd45eaedcd3c3e0486
Roger Swanson <rswansonsc@gmail.com>
2014-04-20 17:50:15 -0400

    Easter homework time...grinding through it

--- Class_15_doctor/app/controllers/offices_controller.rb
+++ Class_15_doctor/app/controllers/offices_controller.rb
@@ -8,7 +8,19 @@ def new
 	@office = Office.new
 end
 
+def create
+  @office = Office.create office_params
+  redirect_to root_path
+end
+
 def edit
+	@office = Office.find params[:id]
+end
+
+def update
+	@office = Office.find params[:id]
+	@office.update_attributes office_params
+	redirect_to root_path
 end
 
 private
